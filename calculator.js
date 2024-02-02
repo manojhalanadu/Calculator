c3ecd1a37e25d22a4bdc2e219e15ea2c4ea01585
Manoj <149938809+manojhalanadu@users.noreply.github.com>
2024-02-02 16:57:04 +0530

    Create isValid expression function
    This function checks whether the expression in the minDisplay is valid before parsing it

--- calculator.js
+++ calculator.js
@@ -3,6 +3,10 @@ const buttons = buttonsContainer.querySelectorAll('.button');
 const minDisplay = document.querySelector('.min-display');
 const paragraphs = Array.from(document.querySelectorAll('.button p'));
 const equals = buttonsContainer.querySelector('#equals');
+const maxDisplay = document.querySelector('.max-display-content');
+const equalSymbol = document.querySelector('.equals-symbol');
+const MAX_NUMBER = 9999999999;
+
 
 
 
@@ -11,6 +15,10 @@ buttonsContainer.addEventListener('click', (event) => {
     if (target.classList.contains('button') || paragraphs.includes(target)) {
 
         textContent = target.textContent.trim();
+        if (maxDisplay.textContent !== '') {
+            minDisplay.textContent = maxDisplay.textContent;
+            maxDisplay.textContent = '';
+        }
         //check whether the button clicked represents a function
         //eg. clearing the display
         if (isAFunctionButton(textContent)) {
@@ -43,16 +51,29 @@ function handleFunctionButtons(functionSymbol) {
     switch (functionSymbol) {
         case 'CE':
             clearMinDisplay();
+            clearMaxDisplay();
+            minDisplay.parentElement.style.borderTop = '';
+            toggleEqualDisplay();
             break;
         case '+/-':
             toggleSign();
             break;
         case '=':
-            const [operand1, operand2, operator] = parseExpression();
-            operate();
+            if (isValidExpression(minDisplay.textContent)) {
+                const [operand1, operand2, operator] = parseExpression();
+                operate(operand1, operand2, operator);
+                //creates a line between the min and max display
+                createSeperationLine();
+            }
             break;
 
     }
+}
+
+function isValidExpression(expression) {
+    const regex = /(-?[.\d]+)([\+-\/x])(-?\+?[.\d+])/;
+    return regex.test(expression);
+    
 }
 
 function clearMinDisplay() {
@@ -60,29 +81,32 @@ function clearMinDisplay() {
 }
 function toggleSign() {
     let minContent = minDisplay.textContent;
-    if (minContent[0] !== '+') {
-        minContent = minContent.replace(/-/g, '');
-        minDisplay.textContent = '+' + minContent;
+    if (minContent[0] == '-') {
+        // minContent = minContent.replace(/-/g, '');
+        minDisplay.textContent = minContent.slice(1);
+
     } else {
-        minDisplay.textContent = '-' + minContent.slice(1);
+        minDisplay.textContent = '-' + minContent;
     }
 }
 
 function operate(operand1, operand2, operator) {
+    let result = '';
     switch (operator) {
         case '+':
-            add(operand1, operand2);
+            result += add(operand1, operand2);
             break;
         case '-':
-            substract(operand1, operand2);
+            result += substract(operand1, operand2);
             break;
         case 'x':
-            multiply(operand1, operand2);
+            result += multiply(operand1, operand2);
             break;
         case '/':
-            divide(operand1, operand2);
+            result += divide(operand1, operand2);
             break;
     }
+    updateMaxDisplay(result);
 }
 
 function add(operand1, operand2) {
@@ -99,4 +123,39 @@ function multiply(operand1, operand2) {
 
 function divide(operand1, operand2) {
     return operand1 / operand2;
+}
+
+function parseExpression() {
+    let minContent = minDisplay.textContent;
+    let regex = /(-?[.\d]+)([\+-\/x])(-?\+?[.\d+])/;
+    let [, operand1, operator, operand2] = regex.exec(minContent);
+    return [operand1, operand2, operator];
+}
+
+function updateMaxDisplay(content) {
+    toggleEqualDisplay();
+    content = +content;
+    if (content > MAX_NUMBER) {
+        content = content.toExponential(3);
+    } else {
+        content = +content.toFixed(4);
+    }
+
+    maxDisplay.textContent = content;
+}
+
+function clearMaxDisplay() {
+    maxDisplay.textContent = '';
+}
+
+function createSeperationLine() {
+    minDisplay.parentElement.style.borderTop = '1px solid white';
+}
+
+function toggleEqualDisplay(){
+    if (equalSymbol.textContent !== '=') {
+        equalSymbol.textContent = '=';
+    } else {
+        equalSymbol.textContent = '';
+    }
 }
